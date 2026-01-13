ad4ff1f2f09b50c4ddc54ac0f654c3e5048040f3
Shreyansh Singh <shreyansh2303@gmail.com>
2026-01-13 10:57:56 +0000

    WIP

---
+++ tests/core/tds/pandas_api/frames/functions/test_expanding_function.py
@@ -0,0 +1,31 @@
+# Copyright 2026 Goldman Sachs
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from pylegend.core.tds.tds_column import PrimitiveTdsColumn
+from pylegend.extensions.tds.pandas_api.frames.pandas_api_table_spec_input_frame import PandasApiTableSpecInputFrame
+
+
+class TestErrors:
+
+    def test_invalid_axis(self):
+        columns = [
+            PrimitiveTdsColumn.integer_column("col1"),
+            PrimitiveTdsColumn.string_column("col2")
+        ]
+        frame: PandasApiTdsFrame = PandasApiTableSpecInputFrame(["test_schema", "test_table"], columns)
+        with pytest.raises(NotImplementedError) as v:
+            frame.groupby("col1", level=1)
+        assert v.value.args[0] == (
+            "The 'level' parameter of the groupby function is not supported yet. "
+            "Please specify groupby column names using the 'by' parameter."
+        )
